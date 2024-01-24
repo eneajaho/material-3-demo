3f8e742967ade4b0c685c07b4ce727b1ba82a08e
Enea Jahollari <jahollarienea14@gmail.com>
2024-01-24 12:13:47 +0100

    add m3, dark mode and density

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, effect, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatOption } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [
+    MatButtonModule,
+    MatDivider,
+    MatIcon,
+    MatCheckbox,
+    MatFormField,
+    MatSelectModule,
+    MatLabel,
+    MatOption,
+  ],
 })
 export class AppComponent {
-  title = 'material-3-demo';
+  darkTheme = signal(false);
+
+  DENSITY_LEVELS = ['maximum', 0, '-1', '-2', '-3', '-4', 'minimum'];
+
+  density = signal(1);
+
+  constructor() {
+    effect(() => {
+      document.body.classList.toggle('dark', this.darkTheme());
+    });
+
+    effect(() => {
+      // remove density-* classes
+      document.body.classList.forEach((className) => {
+        if (className.startsWith('density-')) {
+          document.body.classList.remove(className);
+        }
+      });
+
+      // add density-* class
+      document.body.classList.add(`density-${this.density()}`);
+    });
+  }
 }
